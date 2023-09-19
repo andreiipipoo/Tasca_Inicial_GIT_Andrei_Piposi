b3a4fc36a137195fb71e9fecef32a9ca883508e4
andreipip <andrei.piposi@estudiantat.upc.edu>
2023-09-19 19:17:58 +0200

    Primera parte acabada

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int primero = 10;
+        int segundo = 20;
+
+        // suma de dos números
+        int suma = primero + segundo;
+        System.out.println(primero + " + " + segundo + " = "  + suma);
+
+        // multiplicación de dos números
+
+
+
+    }
+}
