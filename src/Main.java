5966d041593bd6e268172a3a2d635b745dcfd6e0
mariiomc <101208018+mariiomc@users.noreply.github.com>
2023-09-20 18:06:49 +0200

    Multiplicación añadida

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,8 @@ public class Main {
         System.out.println(primero + " + " + segundo + " = "  + suma);
 
         // multiplicación de dos números
-
+        int multiplicacion = primero * segundo;
+        System.out.println("Resultado: " + multiplicacion);
 
 
     }
